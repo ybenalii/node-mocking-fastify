da8fb9c355880cf652c7938ea3ce16e45107ff28
Yasminisasleep <Benaliyasmine904@gmail.com>
2026-04-27 18:42:59 +0100

    let users add new products through a POST endpoint

--- mock-srv/routes/electronics/index.js
+++ mock-srv/routes/electronics/index.js
@@ -1,20 +1,26 @@
 'use strict'
 
+const data = [
+  {
+    id: 'A1',
+    name: 'Filtre de Signal S-1',
+    rrp: '145.00',
+    info: 'Suppression active des bruits résiduels sur circuits haute impédance.'
+  },
+  {
+    id: 'A2',
+    name: 'Modulateur de Flux M-2',
+    rrp: '620.00',
+    info: "Unité d'amplification pour transmission de données longue distance."
+  }
+]
+
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return [
-      {
-        id: 'A1',
-        name: 'Filtre de Signal S-1',
-        rrp: '145.00',
-        info: 'Suppression active des bruits résiduels sur circuits haute impédance.'
-      },
-      {
-        id: 'A2',
-        name: 'Modulateur de Flux M-2',
-        rrp: '620.00',
-        info: "Unité d'amplification pour transmission de données longue distance."
-      }
-    ]
+    return data
+  })
+  fastify.post('/', async function (request, reply) {
+    request.mockDataInsert(opts.prefix.slice(1), data)
+    return data
   })
 }
--- mock-srv/routes/vehicles/index.js
+++ mock-srv/routes/vehicles/index.js
@@ -1,9 +1,15 @@
 'use strict'
 
+const data = [
+  {id: 'B1', name: 'Module C-22', rrp: '890.00', info: 'Transport individuel compact à lévitation magnétique.'}
+]
+
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return [
-      {id: 'B1', name: 'Module C-22', rrp: '890.00', info: 'Transport individuel compact à lévitation magnétique.'}
-    ]
+    return data
+  })
+  fastify.post('/', async function (request, reply) {
+    request.mockDataInsert(opts.prefix.slice(1), data)
+    return data
   })
 }
